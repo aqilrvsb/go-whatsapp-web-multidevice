e1ba67d33f3b12ba0afbd8a7fabad74def5f2680
aqilrvsb <aqilrvsb@example.com>
2025-07-05 11:06:41 +0800

    Revert "Add debug logging to check user_devices columns and use device_name"
    This reverts commit e0d5e661a245f3378caa582d5e08b699a15626a6.

--- commit_fix.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\ROGSTRIX\go-whatsapp-web-multidevice-main
-git commit -m "Fix auto-reconnect database column name issue - use device_name instead of name"
-git push origin main
-echo Done!
-pause
